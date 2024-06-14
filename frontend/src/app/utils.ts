307e8394ba78a00320c38e8d1a51a8a0beac673f
leqing92 <leqing92@hotmail.com>
2024-06-14 16:29:37 +0800

    first commit

--- frontend/src/app/utils.ts
+++ frontend/src/app/utils.ts
@@ -14,19 +14,3 @@ export const dataToImage = (dataUrl: string, filename = 'pic.png') => {
   }
   return new File([u8arr], filename, { type: mime });
 }
-
-export const dataURItoBlob = (dataURI: String, filename = 'pic.png') => {
-  let mimeString = dataURI.split(',')[0].split(';')[0];  //extract content type
-  // console.info("contentType", mimeString);
-  var byteString = atob(dataURI.split(',')[1]); 
-  var ab = new ArrayBuffer(byteString.length)
-  var ia = new Uint8Array(ab)
-
-  // populate the btyestring into blob(btye[])
-  for(var i = 0; i < byteString.length; i++){
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  return new Blob([ab], {type: mimeString});
-}
-
